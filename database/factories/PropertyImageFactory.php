969e53724fb9cc84ea22f60c1ee5c83793637e59
Mansour Abdullah <manodullah@gmail.com>
2026-08-20 20:00:33 +0300

    chore: scaffold migrations, models, factories, seeders, controllers, and form requests

---
+++ database/factories/PropertyImageFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\PropertyImage;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<PropertyImage>
+ */
+class PropertyImageFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
